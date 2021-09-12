e6740fd7f79359a0946d1339210358a43b8c2250
성우 <65459616+ABizCho@users.noreply.github.com>
2021-09-12 07:03:51 +0000

    DataStructure practice

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;  
+  
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Set h = new HashSet();
+    h.add(3);
+    System.out.println("h");
+    /* 1.HashSet : 원소의 중복을 허용하지 않는 Set클래스로 저장순서는 중요하지 않다 */
+    //Hash ds_hashset1 = new HashSet();
+    //ds_hashset1.add(1);
+    //System.out.println(ds_hashset1);
+    
+    
+  }
+}
